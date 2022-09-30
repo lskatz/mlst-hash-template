6ef23f2aeb2e90b73ff0b04ee0b5e580d5f1acc1
Andrea Telatin <15690844+telatin@users.noreply.github.com>
2022-09-30 03:03:13 +0100

    Nim tests and benchmark (#8)
    * Nim implementation
    
    * Ignore rust build
    
    * update nim, tests
    
    * Test WF
    
    Co-authored-by: Lee Katz <gzu2@cdc.gov>

--- nim/run.sh
+++ nim/run.sh
@@ -19,3 +19,14 @@ fi
 echo "CHECK OUTPUT"
 md5sum output-*/alleles* |sort
 md5sum output-*/ref* |sort
+
+hyperfine --version && \
+ mkdir -p benchmark && \
+ hyperfine --export-csv benchmark/speed.csv --export-markdown benchmark/speed.md \
+    --warmup 1 --min-runs 20 --cleanup "rm -rf output*" -n perl -n python -n nim -n nim-buf -n rust \
+    "../scripts/digestFasta.pl --force --out output-pl/ $INPUT" \
+    "../scripts/digestFasta.py --force --out output-py/ $INPUT" \
+    "bin/fu-digest $INPUT --force --out output-nim/" \
+    "bin/fu-digest2 $INPUT --force --out output-nim-buf/" \
+    "../rust/target/release/mlst-hash-template-rust $INPUT"
+rm alleles.tsv ref.fasta 
